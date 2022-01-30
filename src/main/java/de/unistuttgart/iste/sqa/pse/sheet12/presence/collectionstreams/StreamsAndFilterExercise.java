97538aaf5c4046ea589ab3e0f0e26e72f998dce0
franksn90 <s.frankausowen@googlemail.com>
2022-01-30 12:36:53 +0100

    Fix operation names not describing the functionality well

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/presence/collectionstreams/StreamsAndFilterExercise.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/presence/collectionstreams/StreamsAndFilterExercise.java
@@ -1,9 +1,10 @@
 package de.unistuttgart.iste.sqa.pse.sheet12.presence.collectionstreams;
 
 /**
- * In this class you are supposed to train your skills in using the Java Streams API and Collections.
- * Implement the four empty operations in this class properly by using streams.
- * Do not forget to use defensive programming and to comment your code!
+ * In this class you are supposed to train your skills in using the Java Streams
+ * API and Collections. Implement the four empty operations in this class
+ * properly by using streams. Do not forget to use defensive programming and to
+ * comment your code!
  */
 public final class StreamsAndFilterExercise {
 
@@ -12,26 +13,26 @@ public final class StreamsAndFilterExercise {
 		DataGenerator.fillRecordWithTestData(record);
 		DataGenerator.fillRecordWithRealLifePSEMarks(record);
 
-		getStudentName(record);
-		getStudentsNameOlderThan(record, 23);
-		getNumberOfStudentsOlderThan(record, 23);
-		getStudentsFailedExam(record, 25, "PSE");
+		printAllStudentNames(record);
+		printStudentNamesOlderThan(record, 23);
+		printNumberOfStudentsOlderThan(record, 23);
+		printStudentNamesOlderThanAndFailedExam(record, 25, "PSE");
 	}
 
-	private static void getStudentName(final StudentRecord record) {
-		//TODO: implement
+	private static void printAllStudentNames(final StudentRecord record) {
+		// TODO: implement
 	}
 
-	private static void getNumberOfStudentsOlderThan(final StudentRecord record, final int olderThan) {
-		//TODO: implement
+	private static void printNumberOfStudentsOlderThan(final StudentRecord record, final int olderThan) {
+		// TODO: implement
 	}
 
-	private static void getStudentsNameOlderThan(final StudentRecord record, final int olderThan) {
-		//TODO: implement
+	private static void printStudentNamesOlderThan(final StudentRecord record, final int olderThan) {
+		// TODO: implement
 	}
 
-	private static void getStudentsFailedExam(final StudentRecord record, final int olderThan, final String exam) {
-		//TODO: implement
+	private static void printStudentNamesOlderThanAndFailedExam(final StudentRecord record, final int olderThan, final String exam) {
+		// TODO: implement
 	}
 
 }
